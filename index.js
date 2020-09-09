2c53ec709c61839031d2c361de66944254643f92
Keenan McDonald <keemcd@gmail.com>
2020-09-09 17:30:32 -0500

    mobile friendly

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ $(window).on('scroll', function() {
     
     titleScrollOut(scrollTop)
     backgroundScrollIn(scrollTop)
-    coverScrollIn(scrollTop)
+    purchaseScrollIn(scrollTop)
+    aboutScrollIn(scrollTop)
 })
 
 
@@ -17,24 +18,31 @@ function titleScrollOut(scrollTop){
 function backgroundScrollIn(scrollTop){
     let backgroundOpacity = Math.min(scrollTop / 500, 1)
 
-    /*
+    
     const aboutTop = $('#about').position().top
     const scrollBottom = scrollTop + $(window).height();
 
     if (scrollBottom >= aboutTop){
-        backgroundOpacity = 1 + ((aboutTop - scrollBottom + 200) / 500)
+        backgroundOpacity = 1.5 + ((aboutTop - scrollBottom + 200) / 500)
     }
-    */
+    
 
     $('section.content').css("background-color", `rgb(26,26,26,${backgroundOpacity})`)
 }
 
-function coverScrollIn(scrollTop){
+function purchaseScrollIn(scrollTop){
     const purchaseTop = $('#purchase').position().top
     const amount = Math.max((purchaseTop - scrollTop) / $(window).height(), 0)
-    $('div.purchase').css('opacity', 1-amount)
+    $('#purchase').css('opacity', 1-amount)
     $('img.cover').css('top', Math.max(amount-0.2, 0)*160)
-    console.log(1-(Math.min(0.2, amount)*5))
     $('img.cover').css('box-shadow', `-2px 2px 6px 0px rgba(0,0,0, ${1-(Math.min(0.2, amount)*3)})`);
+}
+
+function aboutScrollIn(scrollTop){
+    const aboutTop = $('#about').position().top
+    const amount = Math.max((aboutTop - scrollTop) / $(window).height(), 0)
+    $('#about').css('opacity', 1-amount)
+
+    $('span.author').css('color', `rgb(240,240,240, ${1-amount})`)
 
 }
